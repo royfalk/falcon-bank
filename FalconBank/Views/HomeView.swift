7db1147e475411cd81233374c71821c1fae2d8d2
Roy Falk <r.falk@f5.com>
2023-05-04 09:38:14 +0300

    Add new view - Home View

---
+++ FalconBank/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  FalconBank
+//
+//  Created by Roy Falk on 02/05/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
